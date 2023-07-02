2d3d692c27526f922c53092056aca2aea31cbf5b
Rubrical <filipesalviano@proton.me>
2023-07-02 14:37:51 -0300

    Config file changes and README update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # dotfiles
 My linux configuration files
 
-## adding a autoconfig script soon 
+## How it works:
+- First `git clone git  clone  https://github.com/Rubrical/dotfiles.git`. 
+
+After that, `cd` to the directory, then you must use the command `chmod +x config-setup.sh` and execute it with `./config-setup.sh`. What will happen next is the full configuration of the system. The father directory will not delete itself, and if you already have the same config files it'll be saved as a backup, once the script uses `mv -b`command instead of some kind of `cp`. 
+
+Any issues feel free to  DM me on my discord: rubricalveil_831
--- config-setup.sh
+++ config-setup.sh
@@ -5,34 +5,40 @@ nc="--noconfirm"
 
 # inicializing: 
 printf "Starting installations\n"
+Sleep 5
 echo "Installaling Yay"
+Sleep 5
 
 # Installaling Yay
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S ${nc} --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si ${nc}
 yay -Y --gendb
 yay -Syu ${nc} --devel
 yay -Y --devel --save
 yay -Syu ${nc}
 
 printf "Installaling system programs\n"
+Sleep 5
 
 # system programs 
 sudo pacman -S ${nc} qtile alacritty kitty starship feh pavucontrol sxhkd rofi volumeicon dunst
 
 printf "Installaling personal programs\n"
+Sleep 5
 
 # personal programs
 sudo pacman -S ${nc} flameshot notepadqq vlc mvp neovim gparted okular galculator freedownloadmanager telegram-desktop spotify manuskript ristretto 
 
 # fonts
 printf "Installaling fonts\n"
+Sleep 5
 
 yay -S ${nc} ttf-jetbrains-mono-nerd-font ttf-jetbrains-mono ttf-font-family
 
 
 # more personal programs
 printf "Installaling the aur packeges\n"
+Sleep 5
 
 yay -S ${nc} discord brave onlyoffice-documentserver-bin picom-git
 
@@ -42,12 +48,17 @@ echo eval "$(starship init bash)" >> ~/.bashrc
 
 # configuring aliases
 {
-    alias ls="ls --color=auto";
+    alias ls="ls -a --color=auto";
     alias update="sudo pacman -Syyuu";
     alias vim=nvim;            
 } >> ~/.bashrc
 
+Sleep 5
+
 # cloning files from github
+printf "Putting it all together\n"
+Sleep 5
+
 cd ~/Downloads/ && git clone https://github.com/Rubrical/dotfiles.git && cd dotfiles/.config && mv -b qtile/ picom/ rofi/ sxhdk/ neofetch/ nvim/ starship.toml ~/.config_sex
 rm -rf ~Downloads/dotfiles
 
@@ -59,6 +70,7 @@ cd ~/Downlodas/archlinux-logout/usr/share && mv -b archlinux-betterlockscreen/ a
 cd ~/Downlodas/archlinux-logout/usr/share/applications && mv -b archlinux-betterlockscreen.desktop /usr/share/applications/
 
 printf "Please reboot to save changes\n"
+Sleep 2
 read -r "Do you wish to reboot the computer? (Y/n) " choice
 case "$choice" in
     y|Y|"" ) sudo reboot;;
